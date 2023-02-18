a27c730ebd1416ab6b74abf27e770793eb0650f9
Linas Norkus <linasnorkus92@gmail.com>
2023-02-18 20:48:20 +0200

    added connections to pages

---
+++ Pizza/Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pizza.Pages.Checkout
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
