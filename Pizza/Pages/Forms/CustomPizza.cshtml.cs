5dc8d6a43e9c1beaeb29eee4dce0994a5df46a71
Linas Norkus <linasnorkus92@gmail.com>
2023-02-20 16:42:53 +0200

    Added functionality to "Custom Pizza" with IActionResult OnPost Method. Now redirects to Checkout page

--- Pizza/Pages/Forms/CustomPizza.cshtml.cs
+++ Pizza/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,29 @@ namespace Pizza.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
         }
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+            if (Pizza.TomatoeSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Onions) PizzaPrice += 1;
+            if (Pizza.GardenRocket) PizzaPrice += 1;
+            if (Pizza.Basil) PizzaPrice += 1;
+            if (Pizza.Pepperoni) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 1;
+            if (Pizza.Tuna) PizzaPrice += 1;
+            if (Pizza.Ham) PizzaPrice += 1;
+            if (Pizza.Beef) PizzaPrice += 1;
+            if (Pizza.Chicken) PizzaPrice += 1;
+            if (Pizza.Prosciutto) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new {Pizza.PizzaName, PizzaPrice});
+        }
     }
 }
+
